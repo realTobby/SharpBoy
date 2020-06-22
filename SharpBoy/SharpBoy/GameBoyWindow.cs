cd0d6deb09d3795c6b29fc9c1d0f4042acf6212d
realTobby <tkattanek00@gmail.com>
2020-06-22 18:45:14 +0200

    Window
    Test Window with SFML

---
+++ SharpBoy/SharpBoy/GameBoyWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SharpBoy
+{
+    public class GameBoyWindow
+    {
+        public void Start()
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(160, 144), "test");
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+            window.SetActive();
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(cs);
+                window.Display();
+            }
+        }
+
+
+    }
+}
--- SharpBoy/SharpBoy/Program.cs
+++ SharpBoy/SharpBoy/Program.cs
@@ -7,6 +7,8 @@ namespace SharpBoy
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            GameBoyWindow gbw = new GameBoyWindow();
+            gbw.Start();
         }
     }
 }
